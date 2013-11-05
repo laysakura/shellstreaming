0ae828fad10b7b8277f734afb8a292744783a907
Sho Nakatani <lay.sakura@gmail.com>
2013-11-05 13:55:33 +0900

    adds randint Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

---
+++ shellstreaming/inputstream/randint.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+    shellstreaming.inputstream.randint
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Generates random integer sequence.
+
+    Mainly for testing `InfiniteStream <#shellstreaming.infinitestream.base.Infinitestream>`_
+"""
+import random
+import time
+from shellstreaming.inputstream.base import InfiniteStream
+from shellstreaming.record import Record
+from shellstreaming.recorddef import RecordDef
+
+
+class RandInt(InfiniteStream):
+    """Infinite input stream which generates random integer sequence"""
+    def __init__(self, min_int, max_int, batch_span_ms=1000):
+        """Constructor
+
+        :param min_int:       minimum integer to generate
+        :param max_int:       maximum integer to generate
+        :param batch_span_ms: time span to assemble records as batch
+        """
+        self._min = min_int
+        self._max = max_int
+        InfiniteStream.__init__(self, batch_span_ms)
+
+    def run(self):
+        rdef = RecordDef([{'name': 'num', 'type': 'INT'}])
+        while True:
+            time.sleep(0.001)
+            if self.interrupted():
+                break
+            self.add(Record(rdef, random.randint(self._min, self._max)))
---
+++ shellstreaming/test/inputstream/test_randint.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from nose.tools import *
+import time
+from shellstreaming.inputstream.randint import RandInt
+
+
+def test_randint_usage():
+    n_batches = 2
+    stream = RandInt(0, 10, batch_span_ms=100)
+    for i_batch, batch in enumerate(stream):
+        for i_record, record in enumerate(batch):
+            assert_greater_equal(record[0], 0)
+            assert_less_equal   (record[0], 10)
+        print('batch#%d has %d records' % (i_batch, i_record + 1))
+
+        n_batches -= 1
+        if n_batches == 0:
+            stream.interrupt()
