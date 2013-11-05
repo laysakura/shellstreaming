e099da0b0e94fd3c05afc15703bf9745fb1abcb2
Sho Nakatani <lay.sakura@gmail.com>
2013-11-05 10:07:55 +0900

    removes tests which requires input from user Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

--- shellstreaming/test/inputstream/test_stdin.py
+++ shellstreaming/test/inputstream/test_stdin.py
@@ -5,9 +5,10 @@ from shellstreaming.inputstream.stdin import Stdin
 
 
 def test_stdin_usage():
-    stream = Stdin(batch_span_ms=1000)
-    for batch in stream:
-        print('a batch contents ----')
-        for record in batch:
-            eq_(len(record), 1)
-            print(record)
+    pass
+    # stream = Stdin(batch_span_ms=1000)
+    # for batch in stream:
+    #     print('a batch contents ----')
+    #     for record in batch:
+    #         eq_(len(record), 1)
+    #         print(record)
