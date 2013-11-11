d62db9677315032f2c976b3f8ae5c4254ff3e303
Sho Nakatani <lay.sakura@gmail.com>
2013-11-11 14:21:37 +0900

    making inputstream becomes asynchronous Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

--- shellstreaming/test/test_master_worker_comm.py
+++ shellstreaming/test/test_master_worker_comm.py
@@ -2,6 +2,7 @@ from nose.tools import *
 import rpyc
 from multiprocessing import Process
 import time
+import os
 
 from shellstreaming.inputstream.textfile import TextFile
 
@@ -9,8 +10,13 @@ from shellstreaming.inputstream.textfile import TextFile
 class InputStreamStarterService(rpyc.Service):
     class exposed_InputStreamStarter(object):
         def __init__(self, callback, inputstream, args):
-            module = __import__('shellstreaming.inputstream.textfile', globals(), locals(), [inputstream], -1)
-            stream = eval('%s %s' % (inputstream, args))
+            self._callback    = callback
+            self._inputstream = inputstream
+            self._args        = args
+
+        def exposed_start(self):
+            module = __import__('shellstreaming.inputstream.textfile', globals(), locals(), [self._inputstream], -1)
+            stream = eval('%s %s' % (self._inputstream, self._args))
 
             n_records = 0
             for batch in stream:
@@ -20,7 +26,7 @@ class InputStreamStarterService(rpyc.Service):
                     eq_('line ', line[0:5])
                     ok_(0 <= int(line[5:]) < 100)  # record order in a batch is not always 'oldest-first'
                     n_records += 1
-            callback(n_records)
+            self._callback(n_records)
 
 
 process = None
@@ -28,13 +34,14 @@ process = None
 
 def _start_worker_thread():
     from rpyc.utils.server import ThreadedServer as Server  # can be ThreadedServer or ThreadPoolServer for performance
-    print('strating worker server')
+    print('[pid=%d] strating worker server' % (os.getpid()))
     Server(InputStreamStarterService, port=18871).start()
 
 
 def setup():
     global process
     # setting up worker
+    print('[pid=%d] starting worker process' % (os.getpid()))
     process = Process(target=_start_worker_thread)
     process.start()
     time.sleep(1)
@@ -42,15 +49,20 @@ def setup():
 
 def teardown():
     global process
+    print('[pid=%d] terminating worker process' % (os.getpid()))
     process.terminate()
 
 
 bgsrv = conn = None
+stop = False
 
 
-def f(number_of_lines):
-    global bgsrv, conn
-    print 'finish!!! (%d lines)' % (number_of_lines)
+def f(number_of_lines):  # masterの呼び出すcallback
+    global bgsrv, conn, stop
+    print '[pid=%d] finish!!! (%d lines)' % (os.getpid(), number_of_lines)
+    stop = True
+    bgsrv.stop()
+    conn.close()
 
 
 def test_jobdispatcher_makes_worker_input_file():
@@ -60,14 +72,17 @@ def test_jobdispatcher_makes_worker_input_file():
     # eval('os.path')は成功した
     bgsrv = rpyc.BgServingThread(conn)
 
-    obj = conn.root.InputStreamStarter(
+    starter = conn.root.InputStreamStarter(
         f,
         'TextFile',
         ('/home/nakatani/git/shellstreaming/shellstreaming/test/inputstream/test_textfile_input01.txt', )
     )
 
-    print('can do everything here!!')
-    time.sleep(3)
+    # async
+    astart = rpyc.async(starter.start)
+    astart()
 
-    bgsrv.stop()
-    conn.close()
+    global stop
+    while not stop:
+        print('[pid=%d] waiting for worker\'s inputstream to finish' % (os.getpid()))
+        time.sleep(0.1)
