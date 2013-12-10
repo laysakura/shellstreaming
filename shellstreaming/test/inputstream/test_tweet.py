94c8f3936dc3ebdb74e38b0a1662d21003f4beeb
Sho Nakatani <lay.sakura@gmail.com>
2013-12-10 17:12:15 +0900

    Removes cnf file containing my consumer secret, ... Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

--- shellstreaming/test/inputstream/test_tweet.py
+++ shellstreaming/test/inputstream/test_tweet.py
@@ -1,23 +1,28 @@
 # -*- coding: utf-8 -*-
 from nose.tools import *
-from os.path import abspath, dirname, join
+from os.path import abspath, dirname, join, exists
 from shellstreaming.config import Config
 from shellstreaming.inputstream.tweet import Tweet
 
 
-config = None
-
-
-def setup():
-    global config
+def test_tweet_usage():
+    # To fully pass this test, create 'shellstreaming/test/data/shellstreaming_test_tweet.cnf' whose contents are:
+    #
+    # [inputstream.tweet]
+    # consumer_key = <your consumer key>
+    # consumer_secret = <your consumer secret>
+    # access_token = <your access token>
+    # access_token_secret = <your access token secret>
+    # public_tweets_url = https://stream.twitter.com/1.1/statuses/sample.json
+    #
     confpath = join(abspath(dirname(__file__)), '..', 'data', 'shellstreaming_test_tweet.cnf')
+    if not exists(confpath):
+        return
+
     config = Config.instance()
     config.set_config_file(confpath)
 
-
-def test_tweet_usage():
-    global config
-    n_batches = 20
+    n_batches = 5
     stream = Tweet(
         # [todo] - use config for not showing my keys & secrets
         consumer_key=config.get('inputstream.tweet', 'consumer_key'),
