0897e00824728ecbda12f821b38568531bd91922
Sho Nakatani <lay.sakura@gmail.com>
2013-12-12 09:37:44 +0900

    updates master and worker executable

--- shellstreaming/comm/master.py
+++ shellstreaming/comm/master.py
@@ -3,9 +3,13 @@
     shellstreaming.comm.master
     ~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    :synopsis: Master process
+    :synopsis: Provides master process's entry point
 """
 
 
 def main():
+    """Master process's entry point.
+
+    :returns: exit status of master process
+    """
     print('hello from master')
--- shellstreaming/comm/worker.py
+++ shellstreaming/comm/worker.py
@@ -3,9 +3,13 @@
     shellstreaming.comm.worker
     ~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    :synopsis: worker process
+    :synopsis: Provides worker process's entry point
 """
 
 
 def main():
+    """Worker process's entry point.
+
+    :returns: exit status of worker process
+    """
     print('hello from worker')
