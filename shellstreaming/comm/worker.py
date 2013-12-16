02ad075cf3271b9b4ca7e806600f3ed06373a9e4
Sho Nakatani <lay.sakura@gmail.com>
2013-12-16 18:44:06 +0900

    adds config parser for worker process

--- shellstreaming/comm/worker.py
+++ shellstreaming/comm/worker.py
@@ -8,14 +8,12 @@
     .. note::
         This script is supposed to run via `python` command in worker nodes.
 """
+import argparse
 import sys
 import shlex
 import os
-import time
 from os.path import abspath, dirname, join
 from subprocess import Popen, STDOUT
-from threading import Thread
-from rpyc.utils.server import ThreadedServer as Server
 from ConfigParser import SafeConfigParser as Config
 from shellstreaming.comm.worker_server import WorkerServerService
 
@@ -24,32 +22,27 @@ config = Config()
 config.read('/home/nakatani/git/shellstreaming/shellstreaming/test/data/shellstreaming.cnf')
 
 
-def main(port):
+def main(port, cnfpath):
     """Worker process's entry point.
 
     :param port: TCP port number to launch worker server
+    :param cnfpath: path to config file
     :returns: exit status of worker process
     """
-    if sys.argv[1] == 'async_start_server':
-        # fork this script (on background) and call `start_server()`
-        _async_start_server(port)
-    elif sys.argv[1] == 'run_server':
-        # (forked process) run server
-        _run_server(port)
-    else:  # pragma: no cover
-        assert(False)
+    _async_start_server(port, cnfpath)
 
     return 0
 
 
-def _async_start_server(port):
+def _async_start_server(port, cnfpath):
     global config
-    this_script = abspath(__file__)
-    deploy_dir  = join(dirname(this_script), '..', '..', '..')
+    script     = join(dirname(abspath(__file__)), 'run_worker_server.py')
+    deploy_dir = join(dirname(abspath(__file__)), '..', '..', '..')
     virtualenv_activator = join(deploy_dir, 'bin', 'activate')
-    cmd = 'nohup sh -c ". %(virtualenv)s ; python %(script)s run_server %(port)d" &' % {
+    cmd = 'nohup sh -c ". %(virtualenv)s ; python %(script)s --config=%(cnfpath)s --port=%(port)d" &' % {
         'virtualenv' : virtualenv_activator,
-        'script'     : this_script,
+        'script'     : script,
+        'cnfpath'    : cnfpath,
         'port'       : port,
     }
 
@@ -57,25 +50,26 @@ def _async_start_server(port):
           # stderr=STDOUT, stdout=WorkerServerService.logger  # [todo] - redirect to logger
     )
 
-    WorkerServerService.logger.debug('[%s async_start_server] Start new process: "%s"'  % (sys.argv[0], cmd))
+    WorkerServerService.logger.debug('Start new process: "%s"'  % (cmd))
 
 
-def _run_server(port):
-    WorkerServerService.logger.debug('[%s run_server] Launching `WorkerServerService` on port %d ...' % (sys.argv[0], port))
+def parse_args():
+    parser = argparse.ArgumentParser('shellstreaming worker entry poing')
 
-    WorkerServerService.server = Server(WorkerServerService, port=port, logger=WorkerServerService.logger)
-    t = Thread(target=WorkerServerService.server.start)
-    t.daemon = True
-    t.start()
+    parser.add_argument(
+        '--config', '-c',
+        required=True,
+        help='Configuration file')
+    parser.add_argument(
+        '--port', '-p',
+        type=int,
+        required=True,
+        help='TCP port number to run server')
 
-    while WorkerServerService.server:
-        # wait for `server` to be `close()`ed by master the client.
-        time.sleep(1.0)
+    args = parser.parse_args()
+    return args
 
-    WorkerServerService.logger.debug('`WorkerServerService` has been closed.')
-    t.join()
 
 if __name__ == '__main__':
-    assert(len(sys.argv) == 3)  # [fix] parse args more efficiently
-    assert(sys.argv[1] in ('async_start_server', 'run_server'))
-    sys.exit(main(port=int(sys.argv[2])))
+    args = parse_args()
+    sys.exit(main(port=args.port, cnfpath=args.config))
