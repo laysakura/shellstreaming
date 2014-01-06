abb6097a4ce600309d88ef28dbac6a817e1abe05
Sho Nakatani <lay.sakura@gmail.com>
2014-01-06 11:27:22 +0900

    separate scheduler loop

--- shellstreaming/scheduler/worker_main.py
+++ shellstreaming/scheduler/worker_main.py
@@ -16,14 +16,8 @@ def sched_loop(
     reschedule_interval_sec,
 ):
     """Scheduler main loop of stream processing
-
-    Loop of 1-3.
-
-    1. calculate next job placement from current job placement, machine resource usage, ...
-    2. request each worker next job placement
-    3. sleep
     """
     sched_module = import_module(sched_module_name)
     while True:
         sched_module.update_instances(job_graph, ws.REGISTERED_JOBS, ws.job_instances)
-        time.sleep(0.1)  # [fix] - consider sleep time
+        time.sleep(reschedule_interval_sec)
