83eff809d76e34b510ca8da9ebdc017e2c37b479
Sho Nakatani <lay.sakura@gmail.com>
2014-01-07 11:52:32 +0900

    removes original error classes

--- shellstreaming/test/test_timestamp.py
+++ shellstreaming/test/test_timestamp.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from nose.tools import *
 import datetime
-from shellstreaming.error import UnsupportedTypeError
 from shellstreaming.timespan import Timespan
 from shellstreaming.timestamp import Timestamp
 
